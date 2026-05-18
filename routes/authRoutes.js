a0aff7c5393a5528fd6c01df3744a271fc04c0b4
krishna Sharma <krishnasharma02309@gmail.com>
2026-05-18 19:42:31 +0530

    otpverify

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router()
-const { registerUser, loginUser, getUsers} = require("../controller/authController.js")
+const { registerUser, loginUser, getUsers, verifyOtp} = require("../controller/authController.js")
 const {protect} = require('../middleware/authmiddleware.js')
 const {admin} = require("../middleware/admin.middleware.js")
 
@@ -16,6 +16,6 @@ const {admin} = require("../middleware/admin.middleware.js")
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.get("/user", protect, admin, getUsers)
-
+router.post("/verify-otp", verifyOtp)
 
 module.exports = router
